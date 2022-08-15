2ea5b38175c2ef5b883832a0f7929fc9d03816b2
AgataM <agata.majer@wp.pl>
2022-08-15 13:38:29 +0200

    Added updating and deleting stories

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const connectDB = require('./config/db')
 const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
 const passport = require('passport')
 const session = require('express-session')
 const MongoStore = require('connect-mongo')
@@ -18,13 +19,44 @@ connectDB()
 
 const app = express()
 
+//Body parser
+app.use(express.urlencoded({extended: false}))
+app.use(espress.json())
+
+// Method override
+app.use(
+    methodOverride(function (req, res) {
+      if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        // look in urlencoded POST bodies and delete it
+        let method = req.body._method
+        delete req.body._method
+        return method
+      }
+    })
+  )
+
 //Logging
 if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'))
 }
 
+//Handlebars Helpers
+const { formatDate, stripTags, truncate, editIcon, select} = require('./helpers/hbs')
+
 //Handlebars
-app.engine('.hbs', exphbs.engine({defaultLayout: 'main', extname: '.hbs'}))
+app.engine(
+    '.hbs', 
+    exphbs({
+        helpers: {
+            formatDate,
+            stripTags,
+            truncate,
+            editIcon,
+            select
+        },defaultLayout: 'main',
+         extname: '.hbs',
+    })
+)
 app.set('view engine', '.hbs')
 
 // Sessions 
@@ -41,6 +73,11 @@ app.use(session({
 app.use(passport.initialize())
 app.use(passport.session())
 
+//Set global variable
+app.use(function(req, res, next) {
+    res.localsuser = req.user || null
+    next()
+})
 
 //Static folder
 //app.use(express.static(path.join(__dirname, 'public')))
